d0c687bfc06cb61b89f9dde99cde1e443baefefa
unknown <anujraghuwanshi2407@gmail.com>
2023-08-17 12:36:21 +0530

    avc

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,10 +8,10 @@ const Modal = ({show , onClose}) => {
    console.log(show)
    return(
     <div className="model-overlay">
-       <div  className="model-close">
-       <button onClick={onClose}>Close
-       </button>
-      <p className="model p">This is the content of the modal</p>
+       <div  className="model-close" onClick={onClose}>
+               Close
+      
+        <p className="model p">This is the content of the modal</p>
       </div>
        
     </div>
